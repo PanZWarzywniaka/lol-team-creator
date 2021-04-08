c813be67165ceee43598c69069661351db62bee7
olek <tupolewcx@gmail.com>
2021-04-08 18:30:09 +0200

    Controller now fetches the summoner names and enthers them in a has

--- controllers/index.rb
+++ controllers/index.rb
@@ -6,3 +6,26 @@ end
 get "/index" do
     erb :index
 end
+
+post "/index" do
+
+    map = fetchSummoners(params)
+
+
+    redirect "/index"
+end
+
+def fetchSummoners(params)
+    ret = Hash.new
+
+
+    for i in 0..9 do
+
+        name = params["summoner_#{i}"]
+        puts "Summoner number #{i+1}: #{name}"
+        ret[name] = nil
+    end
+
+    puts ret
+    ret
+end
